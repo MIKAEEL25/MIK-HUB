b871e8ae1b039f15b6eb015c34d472ce1282b4d4
MikaeelSmaili <mikaeelsmaili25167@gmail.com>
2025-12-16 00:19:04 +0330

    improve favorite page

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -7,17 +7,36 @@ const Favorites = (): JSX.Element => {
   const favorites = useSelector(
     (state: RootState) => state.rootReducer.favorite
   );
+  const isLoggedIn = useSelector(
+    (state: RootState) => state.rootReducer.auth.isLogin
+  );
   const uniqueArr = [...new Set(favorites)];
-  return (
-    <section className="absolute left-0 text-center right-0">
-      <div className="w-fit m-auto mt-56">
+
+  let content;
+  if (!isLoggedIn) {
+    content = (
+      <p className="text-5xl text-center text-primary animate-pulse">
+        Please Login First!
+      </p>
+    );
+  }
+  if (isLoggedIn) {
+    content = (
+      <>
         {uniqueArr.length === 0 ? (
           'No Favorite Movie'
         ) : (
           <Movie data={uniqueArr} />
         )}
-      </div>
-    </section>
+      </>
+    );
+  }
+  return (
+    <>
+      <section className="absolute left-0 text-center right-0">
+        <div className="w-fit m-auto mt-56">{content}</div>
+      </section>
+    </>
   );
 };
 export default Favorites;
