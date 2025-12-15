e52fa20a51baf8e6f257c77792dd072a5cbb870b
MikaeelSmaili <mikaeelsmaili25167@gmail.com>
2025-12-16 00:03:26 +0330

    fix rout bug

--- src/components/MovieCard/Movie.tsx
+++ src/components/MovieCard/Movie.tsx
@@ -5,7 +5,11 @@ export const Movie: React.FC<{ data: AllMovie[] }> = ({ data }) => {
   return (
     <div className="grid justify-items-center w-fit m-auto grid-rows-2 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-3 gap-30">
       {data.map((m: AllMovie) => (
-        <MovieCard route="" key={m.id} movie={m} />
+        <MovieCard
+          route={m.title ? '/movies/' : '/series/'}
+          key={m.id}
+          movie={m}
+        />
       ))}
     </div>
   );
